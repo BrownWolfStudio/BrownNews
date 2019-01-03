18198907d84873726253aaf38e77945077bdead2
TheBinaryGuy <heet_mehta@hotmail.com>
2019-01-03 12:45:18 +0530

    Update site.js

--- BrownNews/wwwroot/js/site.js
+++ BrownNews/wwwroot/js/site.js
@@ -2,3 +2,30 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+navigator.serviceWorker.getRegistrations().then(
+
+    function(registrations) {
+
+        for(let registration of registrations) { 
+
+            registration.unregister()
+            .then(function() {
+
+              return self.clients.matchAll();
+
+            })
+         .then(function(clients) {
+
+              clients.forEach(client => {
+
+            if (client.url && "navigate" in client){
+
+                client.navigate(client.url));
+
+            }
+
+         });
+
+        }
+
+});
