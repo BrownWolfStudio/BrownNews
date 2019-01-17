1eec19e81be8d99ff70d709977ac1429a58209a4
TheBinaryGuy <heet_mehta@hotmail.com>
2019-01-18 03:15:22 +0530

    Fixed minor bugs and added a feature to convert image uri from http to https.

---
+++ BrownNews/Utilities/HelperMethods.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace BrownNews.Utilities
+{
+    public static class HelperMethods
+    {
+        public static string HttpToHttps(this Uri uri)
+        {
+            string imgUri = null;
+            if (uri != null)
+            {
+                imgUri = new UriBuilder(uri)
+                {
+                    Scheme = Uri.UriSchemeHttps,
+                    Port = -1
+                }.Uri.ToString();
+            }
+            return imgUri;
+        }
+    }
+}
